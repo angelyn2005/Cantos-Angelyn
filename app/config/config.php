d350bbf11e6b1abb659ef5d51b5cd0853e6bb2f2
angelyn2005 <cantosangelyn@gmail.com>
2025-09-19 17:30:59 +0800

    try

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://cantos-angelyn-3.onrender.com/';
+$config['base_url'] 				= '';
 
 /*
 |--------------------------------------------------------------------------
